51b853b3a23ff07d5ba7341d57f43a5886f49ac9
Alex Nodeland <nodeland.alex@gmail.com>
2024-07-05 17:08:25 -0400

    chore: Update footer in shared_components.py

--- src/crewlit/app/components/shared_components.py
+++ src/crewlit/app/components/shared_components.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
-def Footer():
+def Footer():    
     st.divider()
-    st.markdown("Created with ❤️ by [Alex Nodeland](https://github.com/alexnodeland)")
-    st.markdown("[Crewlit GitHub](https://github.com/alexnodeland/crewlit/) | [CrewAI Docs](https://docs.crewai.com)")
-    with st.expander("Feedback"):
-        st.write("Have suggestions for Crewlit? We'd love to hear from you!")
-        st.page_link(
-            "https://github.com/alexnodeland/crewlit/issues",
-            label="Submit Feedback on GitHub",
-            icon="📢"
-        )
+    st.caption("***Created with ❤️ by [Alex Nodeland](https://github.com/alexnodeland)***")
+    st.caption("***Have suggestions? [Submit Feedback](https://github.com/alexnodeland/crewlit/issues)***")
+    st.caption("***Resources: [Crewlit GitHub](https://github.com/alexnodeland/crewlit/) | [CrewAI Docs](https://docs.crewai.com)***")
